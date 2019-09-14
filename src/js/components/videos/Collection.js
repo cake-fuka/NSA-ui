cf6acc9000c09374f82859e47b95e6b8bf09369a
kokoax <yomo4808@gmail.com>
2019-09-14 21:37:39 +0900

    WIP

--- src/js/components/videos/Collection.js
+++ src/js/components/videos/Collection.js
@@ -3,85 +3,81 @@ import React from "react";
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 
-// class Collection extends react.Component {
-//   constructor() {
-//     super();
-//     const page = "0";
-//     const limit = "70";
-//     const AVGLE_COLLECTION_API = `https://api.avgle.com/v1/collections/${page}?limit=${limit}`;
-// 
-//     this.state = {
-//       collection: []
-//     }
-// 
-//     fetch(AVGLE_COLLECTION_API)
-//       .then((resp) => resp.json())
-//       .then((json) => {
-//         this.setState(
-//           {
-//             collection: json.response.collections.map((resp) => resp.title)
-//           }
-//         );
-//       })
-//       .catch((err) => {
-//       });
-//   }
-// 
-//   buildCollection() {
-//     let key = 0;
-//     return (
-//       this.state.collection.map((collection) => (
-//         <a key={key++} href={`/?name=${collection}`}>{collection}</a>
-//       ))
-//     );
-//   }
-// 
-//   render() {
-//     return (
-//       <div>
-//         <form>
-//           <Select
-//             value={yeah}
-//             onChange={}
-//           >
-//             <MenuItem value={"yeah"}>yeah</MenuItem>
-//           </Select>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-// 
+export default class Collection extends React.Component {
+  constructor() {
+    super();
+    const page = "0";
+    const limit = "70";
+    const AVGLE_COLLECTION_API = `https://api.avgle.com/v1/collections/${page}?limit=${limit}`;
 
-export default function Collection () {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    this.state = {
+      collection: [],
+      anchorEl: null
+    }
 
-
-  function handleClick(event) {
-    setAnchorEl(event.currentTarget);
+    fetch(AVGLE_COLLECTION_API)
+      .then((resp) => resp.json())
+      .then((json) => {
+        this.setState(
+          {
+            ...this.state,
+            collection: json.response.collections.map((resp) => resp.title)
+          }
+        );
+      })
+      .catch((err) => {
+      });
   }
 
-  function handleClose() {
-    setAnchorEl(null);
+  setAnchorEl(target) {
+    this.setState(
+      {
+        ...this.state,
+        anchorEl: target
+      }
+    );
   }
 
-  return (
-    <div>
-      <Button
-        onClick={handleClick}
-      >
-        Open Menu
-      </Button>
-      <Menu>
-        <MenuItem>
-          <ListItemText primary="Sent mail" />
-        </MenuItem>
-      </Menu>
-    </div>
-  );
+  handleClick(event) {
+    this.setAnchorEl(event.currentTarget);
+  }
+
+  handleClose(event) {
+    this.setAnchorEl(null);
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.handleClick.bind(this)}
+        >
+          誰の動画みたい？
+        </Button>
+        {
+          <Menu
+            anchorEl={this.state.anchorEl}
+            open={Boolean(this.state.anchorEl)}
+            onClose={this.handleClose.bind(this)}
+          >
+            {
+              this.state.collection.map((c) =>
+                (
+                  <MenuItem key={c} onClick={this.handleClose.bind(this)}>
+                    <ListItemText primary={c} href={`/?name=${c}`}/>
+                  </MenuItem>
+                )
+              )
+            }
+          </Menu>
+        }
+      </div>
+    );
+  }
 }
