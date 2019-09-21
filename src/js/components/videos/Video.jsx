7a696f2cc9b6998b3bcbdd624bc7ecaa8e66d6ec
kokoax <yomo4808@gmail.com>
2019-09-21 23:24:03 +0900

    そういえば動画開く機能つけてなかった

--- src/js/components/videos/Video.jsx
+++ src/js/components/videos/Video.jsx
@@ -5,22 +5,31 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from '@material-ui/core/CardMedia';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+function redirect() {
+  window.open(this.video_url);
+}
 
 const Video = (video, key) => {
 	return (
     <GridListTile cols={1} key={key}>
-	    <Card>
-        <CardContent>
-          <CardMedia
-            component="img"
-            alt={video.title}
-            height="auto"
-            image={video.preview_url.replace("https", "http")}
-            title={video.title}
-          />
-	        <Typography>{video.title}</Typography>
-        </CardContent>
-	    </Card>
+      <ButtonBase
+        onClick={redirect.bind(video)}
+      >
+	      <Card>
+          <CardContent>
+              <CardMedia
+                component="img"
+                alt={video.title}
+                height="auto"
+                image={video.preview_url.replace("https", "http")}
+                title={video.title}
+              />
+	          <Typography>{video.title}</Typography>
+          </CardContent>
+	      </Card>
+      </ButtonBase>
 	  </GridListTile>
   )
 };
